e765cb1b05614115121d40bce5a3ad7c06397ce6
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-01-18 10:04:59 +0900

    ビデオの送信をサーバにできるように実装

--- client.py
+++ client.py
@@ -24,37 +24,40 @@ class Client:
         
     def sendData(self):
         path = "./video/sample-5s.mp4"
-        with open(path, "rb") as f:
-            print(os.SEEK_END)
-            f.seek(0, os.SEEK_END)
-            filesize = f.tell()
-            print(filesize)
-            f.seek(0, 0)
+        try:
+            with open(path, "rb") as f:
+                print(os.SEEK_END)
+                f.seek(0, os.SEEK_END)
+                filesize = f.tell()
+                print(filesize)
+                f.seek(0, 0)
+                
+                if filesize > pow(2, 32):
+                    raise Exception("file size must be below 4GB")
+                
+                filename = os.path.basename(f.name)
+                
+                if self.checkFileType(filename) == "mp4":
+                    print("ファイル拡張子は mp4です")
+                
+                # まずファイルサイズを送信
+                header = self.makeHeader(len(filename), filesize)
+                self.sock.sendall(header)
+                self.sock.sendall(bytes(filename, "utf-8"))
+                message = f.read()
+                print(message)
+                print("sending data.....")
+                self.sock.sendall(message)
+        except KeyboardInterrupt :
+            print("キーボードが押されました。")
             
-            if filesize > pow(2, 32):
-                raise Exception("file size must be below 4GB")
-            
-            filename = os.path.basename(f.name)
-            
-            if self.checkFileType(filename) == "mp4":
-                print("ファイル拡張子は mp4です")
-            
-            # まずファイルサイズを送信
-            header = self.makeHeader(len(filename), filesize)
-            self.sock.sendall(header)
-            
-
-            message = f.read()
-            print(message)
-        self.sock.sendall(message)
-        print("sending data.....")
         self.sock.settimeout(2)
         
         try:
             while True:
-                data = str(self.sock.recv(4096))
+                data = self.sock.recv(4096)
                 if data:
-                    print("Server response : ", data)
+                    print("Server response : ", str(data))
                 else:
                     break
         except TimeoutError:
--- server.py
+++ server.py
@@ -7,6 +7,7 @@ class Server:
         self.port = int(port)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.buffer = 4096
+        self.dpath = "./temp"
         
     def start(self):
         print("Starting up on {} port {}".format(self.address, self.port))
@@ -25,17 +26,36 @@ class Server:
                 header = connection.recv(5)
                 filename_length = int.from_bytes(header[:1])
                 filesize = int.from_bytes(header[1:])
-                print(filename_length, filesize)
-                while True:
-                    data = connection.recv(1400)
-                    if data:
-                        print("Recived data: ", data)
-                    else:
-                        print("No data from:" , client_address)
-                        break
+                if filesize == 0:
+                    raise Exception('No data  to read from client')
+                
+                filename = connection.recv(filename_length).decode()
+                
+                if not os.path.exists(self.dpath):
+                    os.makedirs(self.dpath)
+                
+                # もしそのファイルが既に存在している場合。
+                if not os.path.exists(os.path.join(self.dpath, filename)):
+                    with open(os.path.join(self.dpath, filename), "wb+" ) as f:
+                        while filesize > 0:
+                            data = connection.recv(filesize if filesize  <= self.buffer else self.buffer)
+                            f.write(data)
+                            print("Recived {} bytes".format(len(data)))
+                            filesize -= len(data)
+                            print(filesize)        
+                            if data:
+                                print("Recived data: ", data)
+                            else:
+                                print("No data from:" , client_address)
+                                break
+                else:
+                    print("そのファイル名+pathはすでに存在しています。")
             finally:
                 print("Closing current connection")
                 connection.close()
+      
+               
+        
 
 def main():
     server = Server("0.0.0.0",  9001)
