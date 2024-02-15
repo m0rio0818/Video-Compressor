104667b3953653dcfae189273dd730e8943b6cfb
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-02-15 17:53:46 +0900

    client.tsの修正

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,28 @@
-class Client{
-    
+import { Socket } from "net";
+
+class Client {
+    public HEADER_BYTES_SIZE: number = 64;
+    public buffer: number = 4096;
+    public dPath: string = "./video";
+    public address: string;
+    public server_address: string;
+    public port: number;
+    public server_port: number;
+    public socket : Socket;
+
+    constructor(
+        address: string,
+        server_address: string,
+        port: number,
+        server_port: number
+    ) {
+        this.address = address;
+        this.server_address = server_address;
+        this.port = port;
+        this.server_port = server_port;
+    }
+
+    connect(): void {}
 }
+
+const client = new Client("0.0.0.0", "0.0.0.0", 9050, 9001);
