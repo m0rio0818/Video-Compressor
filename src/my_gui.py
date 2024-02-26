e923ddea0f1bf82685feaa5742b3e2e3b55d9887
m0rio0818 <72071848+m0rio0818@users.noreply.github.com>
2024-02-26 11:36:26 +0900

    ファイル選択していなかったら、選択できない。ファイルを再選択したら、以前のファイル名が綺麗に消えるように修正

--- src/my_gui.py
+++ src/my_gui.py
@@ -1,9 +1,15 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
+from tkinter import messagebox
 import os
 
 class ViewContlloer:
+    resolution_combobox = None
+    combobox = None
+    fileName = None
+    file_label = None
+    
     def __init__(self) -> None:
         self.root = tk.Tk()
         self.root.title(u"Video Comprssor Service")
@@ -15,16 +21,20 @@ class ViewContlloer:
         print(self.width, " x ", self.height)
 
     def getMp4File(self, event):
-        global filename
-        filename = filedialog.askopenfilename(title="mp4ファイルを選択", filetypes=[("MP4 files", "*.mp4")] , initialdir=os.getcwd())
-        print(filename)
-        return filename
+        ViewContlloer.fileName = filedialog.askopenfilename(title="mp4ファイルを選択", filetypes=[("MP4 files", "*.mp4")] , initialdir=os.getcwd())
+        if ViewContlloer.fileName != "":
+            if hasattr(ViewContlloer, "file_label") and ViewContlloer.file_label is not None:
+                ViewContlloer.file_label.destroy()
+            ViewContlloer.file_label = tk.Label(self.root, text=ViewContlloer.fileName, font=("", 15))
+            ViewContlloer.file_label.place(x=self.width//4, y = 45)        
+        print("選択されたファイル: ",ViewContlloer.fileName)
+
 
     def only_numbers(self, char):
         return char.isdigit() and (len(char) <= 4 and len(char) > 0)
 
     def makeCustomInput(self):
-        validate_numeeric = self.root.register(self.sonly_numbers)
+        validate_numeeric = self.root.register(self.only_numbers)
         entryW = tk.Entry(self.root, validate="key", validatecommand=(validate_numeeric, '%S'), width=10)
         entryW.place(x=250, y=200)
         x_label = tk.Label(self.root, text="x", font=("", 15))
@@ -33,40 +43,41 @@ class ViewContlloer:
         entryH.place(x=400, y=200)
         
     def on_resolution_change(self, event):
-        selected_item = resolution_combobox.get()
+        selected_item = ViewContlloer.resolution_combobox.get()
         if selected_item == "custom":
             print("カスタム入力")
             self.makeCustomInput()
         else:
             print("解像度は以下に変更します", selected_item)
-    def on_combobox_selected(self, sevent):
-        try:
-            print(filename)
-        except NameError:
-            print("ファイル名は定義されていません")
             
-        global resolution_combobox
-        selected_item = combobox.get()
+    def on_combobox_selected(self, event):
+        if ViewContlloer.fileName == None:
+            messagebox.showinfo("mp4ファイル選択", "MP4ファイルを選択してください")
+            ViewContlloer.combobox.set("変換方法を選択してください")
+            return
+            
+        selected_item = ViewContlloer.combobox.get()
         if selected_item == "圧縮":
             radio_label = ["high", "normal", "low"]
             radio_var = tk.IntVar()
             for i in range(len(radio_label)):
                 radio = tk.Radiobutton(self.root, value=i, variable= radio_var, text=radio_label[i])
-                radio.place(x = mid, y = 180 + i * 20)
+                radio.place(x = self.width/2, y = 180 + i * 20)
                 print("圧縮だな〜")
             
         elif selected_item == "解像度変更":
             pulldown_label = ["360p", "720p", "1080p", "WQHD", "4K", "custom"]
-            resolution_combobox = ttk.Combobox(self.root, values=pulldown_label, state="readOnly")
-            resolution_combobox.set("解像度を選択してください")
-            resolution_combobox.place(x=300, y= 150)
-            resolution_combobox.bind("<<ComboboxSelected>>", self.on_resolution_change)
+            ViewContlloer.resolution_combobox = ttk.Combobox(self.root, values=pulldown_label, state="readOnly")
+            ViewContlloer.resolution_combobox.set("解像度を選択してください")
+            ViewContlloer.resolution_combobox.place(x=300, y= 150)
+            ViewContlloer.resolution_combobox.bind("<<ComboboxSelected>>", self.on_resolution_change)
             
             print("解像度変更するか〜")
+        
         elif selected_item == "アスペクト比変更":
             self.makeCustomInput()
-            
             print("アスペクト比変更しようかな〜")
+            
         elif selected_item == "MP3変換":
             print("MP3変換しちゃう")
         elif selected_item == "GIF作成":
@@ -74,25 +85,10 @@ class ViewContlloer:
         elif selected_item == "WEBM作成":
             print("WEBMを作成しま〜す!!")
             
-    def convertVideo():
+    def convertVideo(self, event):
         print("変換ボタンが押されました")
         pass
     
-    # def on_window_resize(event):
-    #     global width, height, mid
-    #     self.root.update_idletasks()
-    #     width = self.root.winfo_width()
-    #     height = self.root.winfo_height()
-    #     mid = width // 2
-        
-    #     if (width >= 600):
-    #         file_label.place(x=width//4, y=10)
-    #         Button.place(x=width//2+100, y=10)
-    #         select_label.place(x=width//4, y=100)
-    #         combobox.place(x=width//2+100, y=100)
-    #         Convert.place(x=mid - 50, y=300)
-    #         # radio.place(x = mid)
-
             
     def makeUI(self):
         file_label = tk.Label(self.root, text="mp4ファイルを選択: ", font=("", 15))
@@ -102,23 +98,21 @@ class ViewContlloer:
         Button.bind("<Button-1>", self.getMp4File)
         Button.place(x=self.width // 2 + 100, y = 10)
         
-        select_label = tk.Label(self.root, text="圧縮方法選択: ", font=("", 15))
+        
+        
+        select_label = tk.Label(self.root, text="変換方法: ", font=("", 15))
         select_label.place(x=self.width//4, y = 100)
         
         module = ('圧縮', '解像度変更', 'アスペクト比変更', 'MP3変換', 'GIF作成', 'WEBM作成')
-        combobox = ttk.Combobox(self.root, values=module, state='readonly')
-        combobox.set("変換方法を選択してください")
-        combobox.place(x=self.width//2 + 100, y = 100)
-        
-        combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
+        ViewContlloer.combobox = ttk.Combobox(self.root, values=module, state='readonly')
+        ViewContlloer.combobox.set("変換方法を選択してください")
+        ViewContlloer.combobox.place(x=self.width//2 + 100, y = 100)
+        ViewContlloer.combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
         
         Convert = tk.Button(self.root, text=u'変換', bg='skyblue', width=10)
         Convert.bind("<Button-1>", self.convertVideo)
         Convert.place(x=self.width//2 - 50, y = 300)
-        
-        # # ウィンドウのサイズ変更イベントをバインド
-        # root.bind("<Configure>", on_window_resize)
-        
+               
         self.root.mainloop()
 
 
